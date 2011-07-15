8b88ca952ff562eb692f25cba54263afcbbcfafd
Haldun Bayhantopcu <hb@haldun.me>
2011-07-16 00:37:48 +0300

    Set password in user ini

--- auth/models.py
+++ auth/models.py
@@ -10,6 +10,12 @@ class User(db.Model):
   created = db.DateTimeProperty(auto_now_add=True)
   updated = db.DateTimeProperty(auto_now=True)
 
+  def __init__(self, *args, **kwds):
+    db.Model.__init__(self, *args, **kwds)
+    password = kwds.pop('password', None)
+    if password:
+      self.set_password(password)
+
   def set_password(self, password):
     self.password_hash = bcrypt.hashpw(password, bcrypt.gensalt(log_rounds=1))
 
